2824ecdacc5a70c963677878bb5f9c6ae6f0ca8a
Gabriel Dal Ri <ghdalri@gmail.com>
2021-03-03 18:06:54 -0300

    day3

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   resources :dishes, only: [:index, :show, :edit, :update, :destroy] do
-    resources :orders, only: [:create]
+    resources :orders, only: [:create, :index]
   end
   resources :profiles, only: [:show]
   resources :dashboard, only: [:show]
