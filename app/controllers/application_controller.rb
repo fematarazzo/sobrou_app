09c6e322b0af04e087fcd2775c6d47324a8b5fb9
Felipe Matarazzo <felipemps@protonmail.com>
2021-03-11 19:46:35 -0300

    ajustes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   # Pundit: white-list approach.
   after_action :verify_authorized, except: [:index, :index_today], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: [:index, :index_today], unless: :skip_pundit?
